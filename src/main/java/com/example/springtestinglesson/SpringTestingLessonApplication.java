cf521d560a2e43e138f35cacf41b09e87ce2c328
ksy90101 <ksy90101@gmail.com>
2022-04-23 16:56:25 +0900

    project init

---
+++ src/main/java/com/example/springtestinglesson/SpringTestingLessonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestinglesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestingLessonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtestinglesson/SpringTestingLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestinglesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
